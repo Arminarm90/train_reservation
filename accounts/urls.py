b575b75e77fae165d3483ce900f83ef2d046036e
Armin Mozafary <armin.arm90@gmail.com>
2024-05-13 13:56:46 +0330

    change password urld

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,12 +4,15 @@ from .views import (
     UserRegistrationAPIView,
     TokenRefreshView,
     UserDetailsAPIView,
+    Changepaswword,
 )
 
 urlpatterns = [
     path("login/", LoginAPIView.as_view(), name="api-login"),
     path("register/", UserRegistrationAPIView.as_view(), name="register-login"),
-    path("register/", UserRegistrationAPIView.as_view(), name="register-login"),
     path("profile/", UserDetailsAPIView.as_view(), name="user-profile"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    # change password
+    path("change-password/", Changepaswword.as_view(), name="change-password"),
+    
 ]
