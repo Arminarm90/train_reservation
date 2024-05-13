d840cb6a5628d0331e70f0044f1a944c5e4ace42
Armin Mozafary <armin.arm90@gmail.com>
2024-05-14 01:08:54 +0330

    admin pannel updated

--- train/admin.py
+++ train/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import Station, Route, Schedule, Train, Ticket, Reservation
+from .models import Station, Route, Schedule, Train, Ticket
 
 admin.site.register(Station)
 admin.site.register(Route)
 admin.site.register(Schedule)
 admin.site.register(Train)
 admin.site.register(Ticket)
-admin.site.register(Reservation)
