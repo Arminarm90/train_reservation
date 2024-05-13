1e7bf12235aa3754a283848d687144c10cacc3aa
Armin Mozafary <armin.arm90@gmail.com>
2024-05-13 12:23:56 +0330

    login api view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from api.serializers import LoginSerializer,UserRegistrationSerializer
+from rest_framework.response import Response
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework import status
 
-# Create your views here.
+
+#  Login api
+class LoginAPIView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            email = serializer.validated_data["email"]
+            password = serializer.validated_data["password"]
+            user = authenticate(request, email=email, password=password)
+            if user is not None:
+                refresh = RefreshToken.for_user(user)
+                return Response(
+                    {
+                        "refresh": str(refresh),
+                        "access": str(refresh.access_token),
+                    }
+                )
+            else:
+                # Authentication failed
+                return Response(
+                    {"message": "Invalid credentials"},
+                    status=status.HTTP_401_UNAUTHORIZED,
+                )
+        else:
+            # Invalid data
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
