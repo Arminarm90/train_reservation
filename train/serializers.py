68164a3c0edd9819214de10c90f419b6c1d35fe9
Armin Mozafary <armin.arm90@gmail.com>
2024-05-14 13:53:48 +0330

    ticket list serializer updated

--- train/serializers.py
+++ train/serializers.py
@@ -2,6 +2,15 @@ from .models import Station, Schedule, Route, Train, Ticket, User
 from rest_framework import serializers
 
 class TicketSerializer(serializers.ModelSerializer):
-        class Meta:
-            model = Ticket
-            fields = '__all__'
+    train_name = serializers.CharField(source='schedule.train.name')
+    train_type = serializers.CharField(source='schedule.train.type')
+    origin = serializers.CharField(source='schedule.route.origin.name')
+    destination = serializers.CharField(source='schedule.route.destination.name')
+    capacity = serializers.IntegerField(source='schedule.train.capacity')
+    duration = serializers.DurationField(source='schedule.route.duration')
+    distance = serializers.DecimalField(max_digits=10, decimal_places=2, source='schedule.route.distance')
+    price = serializers.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        model = Ticket
+        fields = ['id', 'train_name','train_type', 'origin', 'destination', 'capacity', 'duration', 'distance', 'price']
