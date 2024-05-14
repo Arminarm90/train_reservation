9e9e6961dce4fff0651908beaf1c0c93c826ce45
Armin Mozafary <armin.arm90@gmail.com>
2024-05-14 11:50:39 +0330

    ticket serializer

--- train/serializers.py
+++ train/serializers.py
@@ -2,4 +2,6 @@ from .models import Station, Schedule, Route, Train, Ticket, User
 from rest_framework import serializers
 
 class TicketSerializer(serializers.Serializer):
-    pass
+        class Meta:
+            model = Ticket
+            fields = '__all__'
