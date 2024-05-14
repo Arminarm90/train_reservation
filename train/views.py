ac9aad1944f79b2b0b342de325c05074b51f58ad
Armin Mozafary <armin.arm90@gmail.com>
2024-05-14 11:50:53 +0330

    ticket list view

--- train/views.py
+++ train/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
+from .models import Ticket
+from rest_framework.response import Response
+from .serializers import TicketSerializer
 
-# Create your views here.
+
+class TicketListAPIView(APIView):
+    def get(self, request):
+        tickets = Ticket.objects.all()
+        serializer = TicketSerializer(tickets, many=True)
+        return Response(serializer.data)
