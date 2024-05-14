181581139d34c73ef56a5bb999140aaf06b3975c
Armin Mozafary <armin.arm90@gmail.com>
2024-05-14 11:55:15 +0330

    train apis debuged

--- train/serializers.py
+++ train/serializers.py
@@ -1,7 +1,7 @@
 from .models import Station, Schedule, Route, Train, Ticket, User
 from rest_framework import serializers
 
-class TicketSerializer(serializers.Serializer):
+class TicketSerializer(serializers.ModelSerializer):
         class Meta:
             model = Ticket
             fields = '__all__'
--- train/urls.py
+++ train/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import TicketListAPIView
 
 urlpatterns = [
-    path('tickets/', TicketListAPIView.as_view(),'tickets')
+    path('tickets/', TicketListAPIView.as_view(), name='ticket-list'),
 ]
