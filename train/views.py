99044c55259401bff729430425ed3ec69f63893e
Armin Mozafary <armin.arm90@gmail.com>
2024-05-16 01:34:20 +0330

    ticket delete method

--- train/views.py
+++ train/views.py
@@ -5,8 +5,9 @@ from rest_framework.permissions import IsAuthenticated
 from .models import Ticket
 from rest_framework.response import Response
 from .serializers import TicketSerializer
+from rest_framework import status
 
-
+# Tickets list
 class TicketListAPIView(APIView):
     def get(self, request):
         # Get query parameters from the request
@@ -25,3 +26,14 @@ class TicketListAPIView(APIView):
             )
         serializer = TicketSerializer(tickets, many=True)
         return Response(serializer.data)
+
+# Tickets Delete
+class TicketDeleteAPIView(APIView):
+    def delete(self, request, pk):
+        try:
+            ticket = Ticket.objects.get(pk=pk)
+        except Ticket.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        ticket.delete()
+        return Response({'message': 'Ticket deleted successfuly'},status=status.HTTP_204_NO_CONTENT)
