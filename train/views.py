f1f5629ee84180bf816244228c7185ea8d142427
Armin Mozafary <armin.arm90@gmail.com>
2024-05-15 13:18:08 +0330

    search function added to ticket list

--- train/views.py
+++ train/views.py
@@ -9,6 +9,19 @@ from .serializers import TicketSerializer
 
 class TicketListAPIView(APIView):
     def get(self, request):
+        # Get query parameters from the request
+        train_name = request.GET.get("train_name")
+        origin = request.GET.get("origin")
+        destination = request.GET.get("destination")
+        # Filter tickets based on the search criteria
         tickets = Ticket.objects.all()
+        if train_name:
+            tickets = tickets.filter(schedule__train__name__icontains=train_name)
+        if origin:
+            tickets = tickets.filter(schedule__route__origin__name__icontains=origin)
+        if destination:
+            tickets = tickets.filter(
+                schedule__route__destination__name__icontains=destination
+            )
         serializer = TicketSerializer(tickets, many=True)
         return Response(serializer.data)
