3f8be953361fdb6f71bcc714e88110ea6ce9e2d6
Armin Mozafary <armin.arm90@gmail.com>
2024-05-16 16:06:35 +0330

    reservation view

--- reservation/views.py
+++ reservation/views.py
@@ -14,9 +14,38 @@ class ReservationCreateAPIView(APIView):
 
     def post(self, request, *args, **kwargs):
         data = request.data.copy()
-        data['user'] = request.user.id  # Add user ID from token
-        serializer = ReservationSerializer(data=data)
+        data["user"] = request.user.id
+
+        ticket_id = data.pop("tickets")
+
+        seat_number = data.pop("seat_number")
+
+        if Reservation.objects.filter(
+            tickets__id=ticket_id, seat_number=seat_number
+        ).exists():
+            return Response(
+                {"error": "The selected seat is already reserved."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        try:
+            ticket = Ticket.objects.get(id=ticket_id)
+        except Ticket.DoesNotExist:
+            return Response(
+                {"error": "The selected ticket is not available."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        reservation_data = {
+            "user": request.user.id,
+            "tickets": ticket.id,
+            "seat_number": seat_number,
+            "number_of_tickets": data.get("number_of_tickets", 1),
+        }
+
+        serializer = ReservationSerializer(data=reservation_data)
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            reservation = serializer.save()
+            response_data = {**serializer.data, "total_price": reservation.total_price}
+
+            return Response(response_data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
